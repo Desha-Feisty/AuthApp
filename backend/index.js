e2b81aed89afc1e923da8ce4d179050952605887
Desha-Feisty <deshafiesty12@gmail.com>
2026-03-07 21:28:31 +0200

    add production build setup and static file serving

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import connectDB from "./db/connectDB.js";
 import dotenv from "dotenv";
 dotenv.config();
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -16,6 +18,12 @@ app.use(
     }),
 );
 app.use("/api/auth", authRoutes);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    });
+}
 
 async function run() {
     try {
